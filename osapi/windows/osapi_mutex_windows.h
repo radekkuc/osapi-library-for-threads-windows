4fc9887724489e7e0e676e48bab8c9d31cb3160f
radekkuc <radekkuc5@gmail.com>
2025-12-27 13:57:31 +0100

    You can track ownership with GetCurrentThreadId, connect Id with locked boolean

--- osapi/windows/osapi_mutex_windows.h
+++ osapi/windows/osapi_mutex_windows.h
@@ -6,20 +6,31 @@ namespace osapi {
 class Mutex : public MutexInterface {
 private:
     HANDLE mutexH;
+    bool locked = false;
+    DWORD id;
 public:
     Mutex() {
         mutexH = CreateMutex(0, false, 0);
     }
+
     virtual ~Mutex() {
         CloseHandle(mutexH);
     }
-    virtual bool lock(unsigned int timeout) {
-        if(!mutexH) return false;
-        WaitForSingleObject(mutexH, timeout);
 
+    virtual bool lock(unsigned int timeout) {
+        id = GetCurrentThreadId();
+        if(!mutexH || locked) return false;
+        if(WaitForSingleObject(mutexH, timeout) == WAIT_OBJECT_0) {
+            locked = true;
+            return true;
+        } 
+        return false;
     }
+
     virtual void unlock() {
+        if(!locked || (GetCurrentThreadId() != id)) return;
         ReleaseMutex(mutexH);
+        locked = false;
     }
 };
 
